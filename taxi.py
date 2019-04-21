bf318499a2233e302f6d2d7c3489d4267927bf51
bivstrog <43310038+bivstrog@users.noreply.github.com>
2019-04-21 13:12:11 +0300

    preparation4

--- taxi.py
+++ taxi.py
@@ -29,3 +29,21 @@ def taxi(coordinatesX_1, coodinatesY_1, coordinatesX_2, coordinatesY_2, clas):
     json_response = response.json()
     
     return json_response
+
+def taxi_zaglushka(coordinatesX_1, coodinatesY_1, coordinatesX_2, coordinatesY_2, clas):
+    return {
+        "currency": "RUB",
+        "distance": 61529.771101536542,
+        "options": [
+            {
+                "class_level": 50,
+                "class_name": "econom",
+                "class_text": "Эконом",
+                "min_price": 495,
+                "price": 10945,
+                "price_text": "10945 руб.",
+                "waiting_time": 203.98798614740372
+            }
+        ],
+        "time": 3816.9397069215775
+    }
