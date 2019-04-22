eed9c920320e0d798b6b66f9d9283ea26221e423
bivstrog <43310038+bivstrog@users.noreply.github.com>
2019-04-22 20:29:57 +0300

    preparation5

--- taxi.py
+++ taxi.py
@@ -1,34 +1,35 @@
 import requests
-
-def take_coor(address):
-    response = None
-    geocoder_request = 'http://geocode-maps.yandex.ru/1.x/?geocode=' + address + '&format=json'
-    response = requests.get(geocoder_request)
-    if response:
-        json_response = response.json()
-        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-        toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
-        toponym_coodrinates = toponym["Point"]["pos"].split()
-        return toponym_address, toponym_coodrinates
-    else:
-        return False, False
-    
 def taxi(coordinatesX_1, coodinatesY_1, coordinatesX_2, coordinatesY_2, clas):
 
     url = ' https://taxi-routeinfo.taxi.yandex.net/taxi_info'
 
     parameters = {
-        "clid": None, # мой код
-        "apikey": None, # мой код
+        "clid": '', # мой код
+        "apikey": '', # мой код
         "rll": str(coordinatesX_1) + ',' + str(coodinatesY_1) + '~' + str(coordinatesX_2) + ',' + str(coordinatesY_2),
         "class": clas, # econom — «Эконом». business — «Комфорт». comfortplus — «Комфорт+». minivan — «Минивен». vip — «Бизнес».
         "lang": "ru"
     }
 
     response = requests.get(url, parameters).json()
-    json_response = response.json()
     
-    return json_response
+    return response
+
+
+def take_coor(address):
+    response = None
+    geocoder_request = 'http://geocode-maps.yandex.ru/1.x/?geocode=' + address + '&format=json'
+    response = requests.get(geocoder_request)
+    json_response = response.json()
+    if json_response['response']['GeoObjectCollection']['metaDataProperty']['GeocoderResponseMetaData']['found'] != '0':
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+        toponym_coodrinates = toponym["Point"]["pos"].split()
+        return toponym_address, toponym_coodrinates
+        #return json_response
+    else:
+        return False, False
+
 
 def taxi_zaglushka(coordinatesX_1, coodinatesY_1, coordinatesX_2, coordinatesY_2, clas):
     return {
@@ -47,3 +48,7 @@ def taxi_zaglushka(coordinatesX_1, coodinatesY_1, coordinatesX_2, coordinatesY_2
         ],
         "time": 3816.9397069215775
     }
+#print(take_coor('ыукенрол'))
+#print(take_coor('Россия, Москва, Уральская, 25'))
+#print(take_coor('Россия, Москва, Первомайская, 42'))
+#print(taxi('37.807584', '55.823565', '37.787111', '55.792265', 'econom'))
